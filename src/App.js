dba8faec3db44f3a80c8d1f017c43b9c5a2ee3bd
SHY <shy_0215@naver.com>
2022-06-29 11:59:10 +0900

    make To Do list (1)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import {useState} from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+
+  console.log(toDos);
   return (
     <div>
-      <h1 className = {styles.title}>Welcome!</h1>
-      <Button text={"continue"} />
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          type={toDo} 
+          placeholder="Write your to do.." 
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
